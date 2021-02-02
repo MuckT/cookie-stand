419122441cd6702e96d2c57d8cf75d3777e3090e
MuckT <mcgtom10@gmail.com>
2021-02-01 19:24:55 -0800

    fix: move render and push to to object creation, put header and footer render methods in functions

--- js/app.js
+++ js/app.js
@@ -32,46 +32,47 @@ function Store(name, minMaxCustomerEachHour, averageCookieSalesPerCustomer) {
       newRow.append(newCell);
     }
   };
+  stores.push(this);
+  this.render();
 }
 
-// Create Store Objects
-let seattle = new Store('Seattle',[23, 65], 6.3);
+// Create Store Objects - Name, [minCustomerPerHour, maxCustomerPerHour], avgCookiesSoldPerCustomer
+let seattle = new Store('Seattle', [23, 65], 6.3);
 let tokyo = new Store('Tokyo', [3, 24], 1.2);
 let dubai = new Store('Dubai', [11, 38], 3.7);
 let paris = new Store('Paris', [20, 38], 2.3);
 let Lima = new Store('Lima', [2, 16], 4.6);
 
-// Push Store Object to 'stores' Array
-stores.push(seattle, tokyo, dubai, paris, Lima);
 
 // Render Hours
-let storeTableHeader = document.querySelector('#sales-table thead');
-let newHeader = document.createElement('th');
-newHeader.innerHTML = '';
-storeTableHeader.append(newHeader);
-for (let i = 0; i < hours.length; i++) {
-  newHeader = document.createElement('th');
-  newHeader.innerHTML = hours[i];
+function renderStoreHeaders(storeHours = hours) {
+  let storeTableHeader = document.querySelector('#sales-table thead');
+  let newHeader = document.createElement('th');
   storeTableHeader.append(newHeader);
-}
-
-// Render All 'store' Objects
-for (let i = 0; i < stores.length; i++) {
-  stores[i].render();
-}
-
-// Render Totals Do After Store Objects are Rendered
-let storeTableFooter = document.querySelector('#sales-table tfoot');
-let newFooter = document.createElement('tr');
-newFooter.innerHTML = 'Totals';
-storeTableFooter.append(newFooter);
-for (let i = 0; i < hours.length; i++) {
-  var total = 0;
-  var totalsToSum = document.querySelectorAll(`tbody tr td:nth-of-type(${i + 1})`);
-  for (let j = 0; j < totalsToSum.length; j++) {
-    total += +totalsToSum[j].innerHTML;
+  for (let i = 0; i < storeHours.length; i++) {
+    newHeader = document.createElement('th');
+    newHeader.innerHTML = storeHours[i];
+    storeTableHeader.append(newHeader);
   }
-  let newFooterCell = document.createElement('td');
-  newFooterCell.innerHTML = `${total}`;
-  newFooter.append(newFooterCell);
 }
+
+function renderStoreFooter(storeHours = hours) {
+  // Render Totals Do After Store Objects are Rendered
+  let storeTableFooter = document.querySelector('#sales-table tfoot');
+  let newFooter = document.createElement('tr');
+  newFooter.innerHTML = 'Totals';
+  storeTableFooter.append(newFooter);
+  for (let i = 0; i < storeHours.length; i++) {
+    var total = 0;
+    var totalsToSum = document.querySelectorAll(`tbody tr td:nth-of-type(${i + 1})`);
+    for (let j = 0; j < totalsToSum.length; j++) {
+      total += +totalsToSum[j].innerHTML;
+    }
+    let newFooterCell = document.createElement('td');
+    newFooterCell.innerHTML = `${total}`;
+    newFooter.append(newFooterCell);
+  }
+}
+
+renderStoreHeaders();
+renderStoreFooter();
