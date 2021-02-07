f6609c1151014c78f13695c1dcaf9a1a83b9aa63
MuckT <mcgtom10@gmail.com>
2021-02-06 20:15:57 -0800

    feat: add store management

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 // Create 'hours' & 'stores' Array
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-const stores = [];
-const totalSaleMatrix = [];
+const stores = JSON.parse(localStorage.getItem('Store_List')) || [];
+let totalSaleMatrix = [];
 
 // Store Constructor
 function Store(name, minMaxCustomerEachHour, averageCookieSalesPerCustomer) {
@@ -24,29 +24,39 @@ function Store(name, minMaxCustomerEachHour, averageCookieSalesPerCustomer) {
   };
   this.calcCookiesSoldEachHour();
   stores.push(this);
+  localStorage.setItem('Store_List', JSON.stringify(stores));
 }
 
-// Create Store Objects - Name, [minCustomerPerHour, maxCustomerPerHour], avgCookiesSoldPerCustomer
-new Store('Seattle', [23, 65], 6.3);
-new Store('Tokyo', [3, 24], 1.2);
-new Store('Dubai', [11, 38], 3.7);
-new Store('Paris', [20, 38], 2.3);
-new Store('Lima', [2, 16], 4.6);
+// Create Store Objects If not present in localStorage - Name, [minCustomerPerHour, maxCustomerPerHour], avgCookiesSoldPerCustomer
+if (localStorage.getItem('Store_List') === null) {
+  new Store('Seattle', [23, 65], 6.3);
+  new Store('Tokyo', [3, 24], 1.2);
+  new Store('Dubai', [11, 38], 3.7);
+  new Store('Paris', [20, 38], 2.3);
+  new Store('Lima', [2, 16], 4.6);
+}
+
 
 // Calculate Totals Using a Matrix
-function calcStoreTotals(localStores = stores) {
+function calcStoreTotals(localStores = JSON.parse(localStorage.getItem('Store_List')), storeHours = hours) {
+  totalSaleMatrix = [];
   let hourlyTotals = [];
   // Create our Matrix of Values
   for (let i = 0; i < localStores.length; i++) {
     // Calculate Store Total Sales
     let totalSales = 0;
-    for (let j = 0; j < hours.length; j ++) {
+    for (let j = 0; j < storeHours.length; j ++) {
       totalSales += localStores[i].cookieSalesPerStore[j];
     }
-    // Push Total Sales to Relevant Cookie Store
-    localStores[i].cookieSalesPerStore.push(totalSales);
+    // Check that we're not pushing additional columns
+    if (localStores[i].cookieSalesPerStore.length <= storeHours.length) {
+      console.log('localStores[i].cookiesSalesPerStore', localStores[i].cookieSalesPerStore);
+      // Push Total Sales to Relevant Cookie Store
+      localStores[i].cookieSalesPerStore.push(totalSales);
+    }
     totalSaleMatrix.push(localStores[i].cookieSalesPerStore);
   }
+  console.table(totalSaleMatrix);
   // Sum All Stores Hourly Total Per Hour
   for(let i = 0; i < totalSaleMatrix[0].length; i++) {
     let hourlyTotal = 0;
@@ -60,7 +70,7 @@ function calcStoreTotals(localStores = stores) {
 }
 
 // Render Table Header
-function renderHeader(storeHours = hours) {
+function renderHeader(storeHours = hours, localTotalSaleMatrix = totalSaleMatrix) {
   let storeTableHeader = document.querySelector('#sales-table thead');
   let newHeader = document.createElement('th');
   storeTableHeader.append(newHeader);
@@ -69,19 +79,21 @@ function renderHeader(storeHours = hours) {
     newHeader.innerHTML = storeHours[i];
     storeTableHeader.append(newHeader);
   }
-  newHeader = document.createElement('th');
-  newHeader.classList += 'total';
-  newHeader.innerText = 'Total';
-  storeTableHeader.append(newHeader);
+  if (storeHours.length + 1 === localTotalSaleMatrix[0].length) {
+    newHeader = document.createElement('th');
+    newHeader.classList += 'total';
+    newHeader.innerText = 'Total';
+    storeTableHeader.append(newHeader);
+  }
 }
 
 // Render Table Body
-function renderBody(localTotalSaleMatrix = totalSaleMatrix) {
+function renderBody(localTotalSaleMatrix = totalSaleMatrix, localStores = JSON.parse(localStorage.getItem('Store_List'))) {
   let storeTableRowElement = document.querySelector('#sales-table tbody');
   // Store Names
-  for (let i = 0; i < stores.length; i++) {
+  for (let i = 0; i < localStores.length; i++) {
     var newRow = document.createElement('tr');
-    newRow.innerHTML = `${stores[i].name}`;
+    newRow.innerHTML = `${localStores[i].name}`;
     storeTableRowElement.append(newRow);
     // Sales Data
     for(let j = 0; j < localTotalSaleMatrix[0].length; j++){
@@ -110,6 +122,19 @@ function renderFooter(localTotalSaleMatrix = totalSaleMatrix) {
   }
 }
 
+function clearTable(){
+  const elementsToClear = [];
+  let storeTableHeader = document.querySelector('#sales-table thead');
+  let storeTableRowElement = document.querySelector('#sales-table tbody');
+  let storeTableFooter = document.querySelector('#sales-table tfoot');
+  elementsToClear.push(storeTableHeader, storeTableRowElement, storeTableFooter);
+  for (let i = 0; i < elementsToClear.length; i++) {
+    while (elementsToClear[i].lastChild) {
+      elementsToClear[i].removeChild(elementsToClear[i].lastChild);
+    }
+  }
+}
+
 function renderAll() {
   renderHeader();
   renderBody();
@@ -119,3 +144,33 @@ function renderAll() {
 calcStoreTotals();
 renderAll();
 
+// Get Form Input
+let myForm = document.querySelector('#store-management form');
+
+// Handle Form Input
+function handleSubmit(event){
+  event.preventDefault();
+  var validEntry = true;
+  var storeName = event.target['store-name'].value;
+  var minCustomerPerHour = +event.target['minimum-customer-per-hour'].value;
+  var maxCustomerPerHour = +event.target['maximum-customer-per-hour'].value;
+  var avgCookiesSoldPerCustomer = +event.target['average-cookies-per-customer'].value;
+  // Prevent Stores with duplicate names from being added
+  for (let i = 0; i < stores.length; i++) {
+    if (stores[i].name === storeName) {
+      validEntry = false;
+      break;
+    }
+  }
+  if (validEntry) {
+    new Store(`${storeName}`, [minCustomerPerHour, maxCustomerPerHour], avgCookiesSoldPerCustomer);
+    clearTable();
+    calcStoreTotals();
+    renderAll();
+  } else {
+    alert('Please Enter a non-duplicate Store Name');
+  }
+}
+
+// Add Form Event Listener
+myForm.addEventListener('submit', handleSubmit);
